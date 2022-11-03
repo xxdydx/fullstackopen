5ae0e6a9c190f06e8766475489feb5c771706c7f
xxdydx <official@arul.me>
2022-11-03 20:24:10 +0800

    not working ://

---
+++ part5/front/src/App.js
@@ -0,0 +1,161 @@
+import { useState, useEffect } from 'react'
+import Blog from './components/Blog'
+import Notif from './components/Notif'
+import NewBlog from './components/NewBlog'
+import Togglable from './components/Togglable'
+import blogService from './services/blogs'
+import loginService from './services/login'
+
+const App = () => {
+  const [blogs, setBlogs] = useState([])
+  const [username, setUsername] = useState('') 
+  const [password, setPassword] = useState('')
+  const [newTitle, setNewTitle] = useState('')
+  const [newAuthor, setNewAuthor] = useState('')
+  const [newUrl, setNewUrl] = useState('')
+  const [notification, setNotification] = useState(null)
+  const [user, setUser] = useState(null)
+
+  useEffect(() => {
+    blogService.getAll().then(blogs =>
+      setBlogs( blogs )
+    )  
+  }, [])
+  
+  useEffect(() => {
+    const loggedUserJSON = window.localStorage.getItem('AKAppSessionID')
+    if (loggedUserJSON) {
+      const user = JSON.parse(loggedUserJSON)
+      setUser(user)
+      blogService.setToken(user.token)
+    }
+  }, [])
+
+  const handleLogin = async (event) => {
+    event.preventDefault()
+
+    try {
+      const user = await loginService.login({username, password})
+      window.localStorage.setItem(
+        'AKAppSessionID', JSON.stringify(user)
+      ) 
+      blogService.setToken(user.token)
+
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    }
+    catch (exception) {
+      setNotification('Wrong credentials')
+      setTimeout(() => {
+        setNotification(null)
+      }, 2500)
+
+    }
+    
+  }
+
+
+  const addBlog = (event) => {
+    const blogObject = {
+      title: newTitle,
+      author:newAuthor,
+      url: newUrl
+    }
+
+    blogService
+    .create(blogObject)
+    .then(response => {
+      setBlogs(blogs.concat.response.data)
+      setNotification ('successfully added blog!')
+      setTimeout(() => {
+        setNotification(null)
+      }, 2500)
+      setNewAuthor('')
+      setNewTitle('')
+      setNewUrl('')
+    })
+    .catch(error =>{
+      setNotification(error.response.data.error)
+      setTimeout(() => {
+        setNotification(null)
+      }, 2500)
+    })
+  }
+
+
+  const logout = (event) => {
+    event.preventDefault()
+    window.localStorage.removeItem('AKAppSessionID')
+    setUser(null)
+  }
+  const loginForm = () => (
+    <div>
+      <h1>Login </h1>
+    <form onSubmit={handleLogin}>
+            <div>
+              username
+                <input
+                type="text"
+                value={username}
+                name="Username"
+                onChange={({ target }) => setUsername(target.value)}
+              />
+            </div>
+            <div>
+              password
+                <input
+                type="password"
+                value={password}
+                name="Password"
+                onChange={({ target }) => setPassword(target.value)}
+              />
+            </div>
+            <button type="submit">login</button>
+          </form>
+    </div>
+  )
+  const blogRendering = () => (
+    <div>
+      <h2>blogs</h2> 
+      <button onClick={logout}>logout</button>
+      <Togglable buttonLabel = 'create'>
+      <NewBlog>
+      addBlog ={addBlog}
+      newAuthor= {newAuthor}
+      newTitle={newTitle}
+      newUrl = {newUrl}
+      titleChange = {({target}) => setNewTitle(target.value)}
+      authorChange = {({target}) => setNewAuthor(target.value)}
+      UrlChange = {({target}) => setNewUrl(target.value)}
+     </NewBlog>
+     </Togglable>
+
+      {blogs.map(blog =>
+        <Blog key={blog.id} blog={blog} />
+      )}
+    </div>
+
+  )
+  
+
+
+
+
+
+  return (
+    <div>
+      <Notif message={notification} />
+      {user === null ?
+      loginForm() :
+      <div>
+        <p>{user.name} logged-in</p>
+        {blogRendering()}
+      </div>
+    }
+
+    </div>
+  )
+}
+
+export default App
---
+++ part5/front/src/components/NewBlog.js
@@ -0,0 +1,31 @@
+const NewBlog = (addBlog, newTitle, titleChange, newAuthor, authorChange, newUrl, UrlChange) => {
+
+    return (
+    <div>
+        <h2>create new</h2>
+
+        <form onSubmit = {addBlog}>
+            <div>
+            title:
+            <input value={newTitle} onChange={titleChange} />
+            </div>
+            <div>
+            author:
+            <input value={newAuthor} onChange={authorChange} />
+            </div>
+            <div>
+            url:
+            <input value={newUrl} onChange={UrlChange} />
+            </div>
+            <div>
+            <button type="submit">add</button>
+            </div>
+      </form>
+    </div>
+
+    )
+
+  
+}
+
+export default NewBlog
