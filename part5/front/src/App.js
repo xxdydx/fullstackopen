1f27de1fc82ab9cd6442db6e49520e73734be53d
xxdydx <official@arul.me>
2022-11-04 20:18:43 +0800

    completed 5.7

--- part5/front/src/App.js
+++ part5/front/src/App.js
@@ -114,9 +114,11 @@ const App = () => {
       <Togglable buttonLabel = 'create' ref={blogFormRef}>
       <NewBlog createBlog={createBlog}/>
      </Togglable>
+     <br/>
+
 
       {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
+        <Blog key={blog.id} blog={blog} user={user} />
       )}
     </div>
 
--- part5/front/src/components/Blog.js
+++ part5/front/src/components/Blog.js
@@ -1,7 +1,34 @@
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import {useState} from 'react'
 
+const Blog = ({blog,user}) => {
+  const [view, setView] = useState(false)
+  const buttonText = view ? "hide" : "view";
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+
+return (
+  <div style={blogStyle}>
+  {blog.title} {blog.author}  <button onClick={() => setView(!view)}>{buttonText}</button>
+            {
+                view ? (
+                    <div>
+                    {blog.url} <br/>
+                    likes {blog.likes} <button>like</button> <br/>
+                    {user.name} <br />
+                    </div>
+                )
+
+                : null
+            }
+
+        </div> 
+   
+)
+}
 export default Blog
