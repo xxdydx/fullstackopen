2881c2eab9c2835b2f044b0193cf2d3ee8ffd720
xxdydx <official@arul.me>
2022-11-07 15:21:57 +0800

    Update counterReducer.test.js

--- part6/reducer/counterReducer.test.js
+++ part6/reducer/counterReducer.test.js
@@ -9,7 +9,6 @@ describe('unicafe reducer', () => {
   }
 
   test('should return a proper initial state when called with undefined state', () => {
-    const state = {}
     const action = {
       type: 'DO_NOTHING'
     }
@@ -22,13 +21,51 @@ describe('unicafe reducer', () => {
     const action = {
       type: 'GOOD'
     }
-    const state = initialState
 
-    const newState = counterReducer(state, action)
+    deepFreeze(initialState)
+    const newState = counterReducer(initialState, action)
     expect(newState).toEqual({
       good: 1,
       ok: 0,
       bad: 0
     })
+  })
+
+  test('bad is incremented', () => {
+    const action = {
+      type: 'BAD'
+    }
+
+    deepFreeze(initialState)
+    const newState = counterReducer(initialState, action)
+    expect(newState).toEqual({
+      good: 0,
+      ok: 0,
+      bad: 1
+    })
+  })
+
+  test('ok is incremented', () => {
+    const action = {
+      type: 'OK'
+    }
+
+    deepFreeze(initialState)
+    const newState = counterReducer(initialState, action)
+    expect(newState).toEqual({
+      good: 0,
+      ok: 1,
+      bad: 0
+    })
+  })
+
+  test('reset to zero', () => {
+    const action = {
+      type: 'ZERO'
+    }
+
+    deepFreeze(initialState)
+    const newState = counterReducer(initialState, action)
+    expect(newState).toEqual(initialState)
   })
 })
